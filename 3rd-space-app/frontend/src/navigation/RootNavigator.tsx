8253d86e8f7af28ccd8247d513da3ec8c78e96ef
Edozie10k <Josephedozie45@gmail.com>
2025-04-05 16:41:41 -0700

    changes

---
+++ 3rd-space-app/frontend/src/navigation/RootNavigator.tsx
@@ -0,0 +1,14 @@
+// src/navigation/RootNavigator.tsx
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import AuthNavigator from './AuthNavigator';
+
+const RootNavigator = () => {
+  return (
+    <NavigationContainer>
+      <AuthNavigator />
+    </NavigationContainer>
+  );
+};
+
+export default RootNavigator;
